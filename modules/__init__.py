6ac0e4f0c9a52719fdaec23936ac44016923ab96
Nick19861111 <114399678+Nick19861111@users.noreply.github.com>
2025-03-26 19:48:49 +0800

    1

--- modules/__init__.py
+++
@@ -1,7 +0,0 @@
-# modules/__init__.py
-from .voice import VoiceRecognizer
-from .weather import WeatherService
-from .speaker import Speaker
-from .nlp import IntentParser
-from  .news import NewsService
-from .utils import listen_while_speaking
